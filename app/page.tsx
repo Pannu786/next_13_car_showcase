91c53dab084f538695f03ce432dcda40a7f4cbe5
Pannu786 <thepannu786@gmail.com>
2023-12-05 23:17:56 +0530

    started doing page cmp

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Hero } from '@/components';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className='overflow-hidden'>
-      <Hero />
-    </main>
+    <div className='hero'>
+      <div className='flex-1 pt-36 padding-x'>
+        <h1 className='hero__title'></h1>
+      </div>
+    </div>
   );
 }
