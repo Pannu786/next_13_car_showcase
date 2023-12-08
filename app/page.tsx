cbe99140ee0dcffdea578531e55bacc444f02424
Pannu786 <thepannu786@gmail.com>
2023-12-08 15:13:25 +0530

    created folder    for types for pros

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,8 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className='hero'>
-      <div className='flex-1 pt-36 padding-x'>
-        <h1 className='hero__title'></h1>
-      </div>
-    </div>
+    <main className='overflow-hidden'>
+      <Hero />
+    </main>
   );
 }
