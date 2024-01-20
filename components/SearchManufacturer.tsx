adedcb6392117da7d9929a300122b1154a1bde50
Pannu786 <thepannu786@gmail.com>
2024-01-20 22:59:40 +0530

    implemented the search vehicle logic

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,16 +1,27 @@
 'use client';
 
+import { useState, Fragment } from 'react';
 import Image from 'next/image';
 import { Combobox, Transition } from '@headlessui/react';
 
+import { manufacturers } from '@/constants';
 import { SearchManufacturerProps } from '@/types';
-import { useState, Fragment } from 'react';
 
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
   const [query, setQuery] = useState('');
+
+  const filteredManufacturer =
+    query === ''
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLowerCase()
+            .replace(/\s+/g, '')
+            .includes(query.toLowerCase().replace(/\s+/g, ''))
+        );
 
   return (
     <div className='search-manufacturer'>
