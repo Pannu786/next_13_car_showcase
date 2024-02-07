f51a7f9aa7f609c8cbe5cbdf13c425189d4059b3
Pannu786 <thepannu786@gmail.com>
2024-02-07 22:13:47 +0530

    changed it Axio to Fetch

--- utils/index.ts
+++ utils/index.ts
@@ -1,9 +1,6 @@
-const axios = require('axios');
-
+const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
 const options = {
   method: 'GET',
-  url: 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
-  params: { model: 'corolla' },
   headers: {
     'X-RapidAPI-Key': '17a9cfd4d4msh6afbbc973cf827fp1e074bjsn672254bc7e1d',
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
@@ -11,8 +8,9 @@ const options = {
 };
 
 try {
-  const response = await axios.request(options);
-  console.log(response.data);
+  const response = await fetch(url, options);
+  const result = await response.text();
+  console.log(result);
 } catch (error) {
   console.error(error);
 }
