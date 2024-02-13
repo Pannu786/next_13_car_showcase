795755652e4a5abb1897419c6f6ee31d0bd57b00
Pannu786 <thepannu786@gmail.com>
2024-02-13 22:50:16 +0530

    fetch function is dne

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,16 @@
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-const options = {
-  method: 'GET',
-  headers: {
+
+export async function fetchCars() {
+  const headers = {
     'X-RapidAPI-Key': '17a9cfd4d4msh6afbbc973cf827fp1e074bjsn672254bc7e1d',
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
-  },
-};
+  };
 
-try {
-  const response = await fetch(url, options);
-  const result = await response.text();
-  console.log(result);
-} catch (error) {
-  console.error(error);
+  const response = await fetch(
+    'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars',
+    { headers: headers }
+  );
+
+  const result = await response.json();
+
+  return result;
 }
