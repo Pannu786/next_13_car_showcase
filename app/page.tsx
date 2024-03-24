0d8345b24c81b4d5a680737e46fc79074882d949
Pannu786 <thepannu786@gmail.com>
2024-03-24 22:07:31 +0530

    created CarCard cmp

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-import { CustomFilter, Hero, SearchBar } from '@/components';
+import { CarCard, CustomFilter, Hero, SearchBar } from '@/components';
 import { fetchCars } from '@/utils';
 
 export default async function Home() {
@@ -29,7 +29,13 @@ export default async function Home() {
         </div>
 
         {!isDataEmpty ? (
-          <section>We have cars</section>
+          <section>
+            <div className='home__cars-wrapper'>
+              {allCars?.map((car) => (
+                <CarCard car={car} />
+              ))}
+            </div>
+          </section>
         ) : (
           <div className='home__error-container'>
             <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
