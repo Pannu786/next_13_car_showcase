f6de761ba69d47dd689ca42fd09dd3bfb79fedca
Pannu786 <thepannu786@gmail.com>
2024-04-13 21:27:40 +0530

    working on cardetails cmp

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -30,7 +30,35 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
           </Transition.Child>
 
           <div className='fixed inset-0 overflow-y-auto'>
-            <div className='flex min-h-full items-center justify-center p-4 text-center'></div>
+            <div className='flex min-h-full items-center justify-center p-4 text-center'>
+              <Transition.Child
+                as={Fragment}
+                enter='ease-out- duration-300'
+                enterFrom='opacity-0 scale-95'
+                enterTo='opacity-100 scale-100'
+                leave='ease-in duration-200'
+                leaveFrom='opacity-100 scale-100'
+                leaveTo='opacity-0 scale-95'
+              >
+                <Dialog.Panel className='relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xsl transition-all flex flex-col gap-5'>
+                  <button
+                    type='button'
+                    className='absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full'
+                    onClick={closeModal}
+                  >
+                    <Image
+                      src='/close.svg'
+                      alt='close'
+                      width={20}
+                      height={20}
+                      className='object-contain'
+                    />
+                  </button>
+
+                  <div className='relative w-full h-40 bg-pattern bg-cover bg-center rounded-lgl'></div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
           </div>
         </Dialog>
       </Transition>
