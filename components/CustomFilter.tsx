24c60e1755b2fb000379b23d3cd41d9137123ab6
Pannu786 <thepannu786@gmail.com>
2024-04-23 22:06:03 +0530

    working on customFilter cmp

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,9 +1,35 @@
 'use client';
 
 import { Fragment, useState } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
-const CustomFilter = () => {
-  return <div>CustomFilter</div>;
+import { Listbox, Transition } from '@headlessui/react';
+import { CustomFilterProps } from '@/types';
+
+const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const [selected, setSelected] = useState(options[0]);
+
+  return (
+    <div>
+      <div className='w-fit'>
+        <Listbox value={selected} onChange={(e) => setSelected(e)}>
+          <div className='relative w-fit z-10'>
+            <Listbox.Button className='custom-filter__btn'>
+              <span className='block truncate'>{selected.title}</span>
+              <Image
+                src='/chevron-up-down.svg'
+                width={20}
+                height={20}
+                className='ml-4 object-contain'
+                alt='chevron up down'
+              />
+            </Listbox.Button>
+          </div>
+        </Listbox>
+      </div>
+    </div>
+  );
 };
 
 export default CustomFilter;
